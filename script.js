520f80a0d65f1a8854705d6cdfb8c9ed7be31035
michelledr <michelled@poshpeanut.com>
2023-08-22 23:38:44 +0800

    Added function to fetch from API on button click

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function generateAdvice(event) {
+async function getAdvice() {
+  const response = await fetch('https://api.adviceslip.com/advice');
+  return response.json();
+}
+
+function generateAdvice() {
   const adviceIdField = document.getElementById("js-advice-id");
   const adviceTextField = document.getElementById("js-advice-text");
+
+  getAdvice().then((advice) => {
+    console.log(advice);
+    adviceIdField.textContent = advice["slip"]["id"];
+    adviceTextField.textContent = advice["slip"]["advice"];
+  });
 }
+
+const generateAdviceBtn = document.getElementById("js-generate-advice-btn");
+generateAdviceBtn.addEventListener("click", generateAdvice);
