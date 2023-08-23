fb3c9c65fe51ebed529de8c4ec11c809effa9101
michelledr <michelled@poshpeanut.com>
2023-08-23 15:33:14 +0800

    Updated README

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-async function getAdvice() {
-  const response = await fetch('https://api.adviceslip.com/advice');
-  return response.json();
-}
-
 function generateAdvice() {
   const adviceIdField = document.getElementById("js-advice-id");
   const adviceTextField = document.getElementById("js-advice-text");
+  const fetchAdvice = fetch('https://api.adviceslip.com/advice');
 
-  getAdvice().then((advice) => {
-    adviceIdField.textContent = advice["slip"]["id"];
-    adviceTextField.textContent = advice["slip"]["advice"];
-  });
+  fetchAdvice
+    .then((response) => response.json())
+    .then((advice) => {
+      adviceIdField.textContent = advice["slip"]["id"];
+      adviceTextField.textContent = advice["slip"]["advice"];
+    });
 }
 
 const generateAdviceBtn = document.getElementById("js-generate-advice-btn");
